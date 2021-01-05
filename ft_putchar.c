9e46e9d0d0024a7f43781260e6a20b1d3571071d
csejault <csejault@e1r4p10.42.fr>
2021-01-05 15:10:46 +0100

    fix: change ft_putchar_fd bu ft_putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: csejault <csejault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 13:00:20 by csejault          #+#    #+#             */
-/*   Updated: 2021/01/05 13:00:24 by csejault         ###   ########.fr       */
+/*   Updated: 2021/01/05 15:09:13 by csejault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
